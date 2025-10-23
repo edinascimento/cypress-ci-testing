955f123a6fc2cb3bcca683891fded9112cb497a6
Edipo Nascimento <esnedipo@gmail.com>
2025-10-23 12:04:20 -0300

    Fix: Use bash -c with single quotes

--- run-cypress.sh
+++ run-cypress.sh
@@ -5,4 +5,4 @@ docker run --rm \
   -v ${WORKSPACE}:/e2e \
   -w /e2e \
   cypress/included:15.5.0 \
-  sh -c "npm ci && npx cypress run --config-file /e2e/cypress.config.js --reporter mochawesome --reporter-options reportDir=cypress/results,overwrite=false,html=false,json=true"
+  bash -c 'npm ci && npx cypress run --config-file /e2e/cypress.config.js --reporter mochawesome --reporter-options reportDir=cypress/results,overwrite=false,html=false,json=true'
