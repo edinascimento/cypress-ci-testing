0137f5ece2fa875b32fc5863f4bf8ac268b54753
Edipo Nascimento <esnedipo@gmail.com>
2025-10-23 14:44:38 -0300

    added bash script

--- run-cypress.sh
+++ run-cypress.sh
@@ -5,4 +5,7 @@ docker run --rm \
   -v ${WORKSPACE}:/e2e \
   -w /e2e \
   cypress/included:15.5.0 \
-  bash -c 'npm ci && npx cypress run --config-file /e2e/cypress.config.js --reporter mochawesome --reporter-options reportDir=cypress/results,overwrite=false,html=false,json=true'
+  npx cypress run \
+    --config-file=/e2e/cypress.config.js \
+    --reporter mochawesome \
+    --reporter-options reportDir=cypress/results,overwrite=false,html=false,json=true
