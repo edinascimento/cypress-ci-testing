df4deb8642820e5f632f1cbd3e037d301238be92
Edipo Nascimento <esnedipo@gmail.com>
2025-10-23 15:03:41 -0300

    Fix: Add sh -c wrapper for docker commands

--- run-cypress.sh
+++ run-cypress.sh
@@ -11,11 +11,11 @@ docker run --rm \
   -v $(pwd):/e2e \
   -w /e2e \
   cypress/included:15.5.0 \
-  ls -la /e2e
+  sh -c "ls -la /e2e"
 
 echo "Running Cypress tests..."
 docker run --rm \
   -v $(pwd):/e2e \
   -w /e2e \
   cypress/included:15.5.0 \
-  npx cypress run --config-file /e2e/cypress.config.js
+  sh -c "npx cypress run --config-file /e2e/cypress.config.js"
