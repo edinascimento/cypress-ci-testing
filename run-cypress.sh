63b3729f71214fed739cfcface1aaa32104ec76c
Edipo Nascimento <esnedipo@gmail.com>
2025-10-23 14:56:56 -0300

    Fix: Separate debug and test docker commands

--- run-cypress.sh
+++ run-cypress.sh
@@ -6,9 +6,16 @@ echo "PWD: $(pwd)"
 echo "Files in current dir:"
 ls -la
 
-echo "Running Docker..."
+echo "Checking files inside container:"
 docker run --rm \
   -v $(pwd):/e2e \
   -w /e2e \
   cypress/included:15.5.0 \
   ls -la /e2e
+
+echo "Running Cypress tests..."
+docker run --rm \
+  -v $(pwd):/e2e \
+  -w /e2e \
+  cypress/included:15.5.0 \
+  npx cypress run --config-file /e2e/cypress.config.js
